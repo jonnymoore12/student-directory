2576956b2ff5991a079a4d9f8fec9245c2ec8100
Jonny Moore <jonnymoore121212@gmail.com>
2016-06-22 13:46:08 +0100

    Updated 9-interactive-menu

--- 9-interactive-menu.rb
+++ 9-interactive-menu.rb
@@ -1,38 +1,31 @@
-@students = []
-
 def interactive_menu
+   students = []
    loop do
-      print_menu
-      process(gets.chomp)
+      #1. Present the options
+      puts "1. Input the students"
+      puts "2. Show the students"
+      puts "9. Exit"
+      #2. Store result as a variable
+      selection = gets.chomp
+      #3. Do what the user has asked
+      case selection
+      when "1"
+         students = input_students
+      when "2"
+         # Show the students
+         print_header
+         cohort_months = cohorts(students)
+         print(students, cohort_months)
+         print_footer(students)
+      when "9"
+         # exit
+         exit
+      else
+         "I didn't catch that. Could you try again please"
+      end
    end
-end
 
-def print_menu
-   puts "1. Input the students"
-   puts "2. Show the students"
-   puts "9. Exit"
 end
-
-def process(selection)
-   case selection
-   when "1"
-      input_students
-   when "2"
-      show_students
-   when "9"
-      exit
-   else
-      puts "I didn't catch that. Can you try again please?"
-   end
-end
-
-def show_students
-   print_header
-   cohort_months = cohorts
-   print_students_list(cohort_months)
-   print_footer
-end
-
 
 
 def input_students
@@ -40,7 +33,7 @@ def input_students
    puts "Please enter in the format: name, cohort, favourite hobby"
    puts "Leave the cohort or hobby field blank if you wish"
    puts "To finish, just hit return twice"
-   @students = []
+   students = []
    name_cohort_hobby = gets.chomp
    while !name_cohort_hobby.empty? do
       while name_cohort_hobby.count(",") != 2
@@ -54,20 +47,20 @@ def input_students
       hobby = name_cohort_hobby.split(",")[2].capitalize || "Unspecified"
       # Get rid of the potential " " after diving commas
       hobby = hobby[1..-1].capitalize if hobby[0] == " "
-      @students << {name: name, cohort: cohort, hobby: hobby}
-      if @students.count == 1
+      students << {name: name, cohort: cohort, hobby: hobby}
+      if students.count == 1
          puts "We now have 1 student"
       else
-         puts "We now have #{@students.count} students"
+         puts "We now have #{students.count} students"
       end
       name_cohort_hobby = gets.chomp
    end
+   students
 end
 
-def cohorts
-   cohort_months = @students.map {|x| x[:cohort]}.uniq
+def cohorts(students)
+   cohort_months = students.map {|x| x[:cohort]}.uniq
 end
-
 
 def print_header
    linewidth = 100
@@ -75,10 +68,13 @@ def print_header
    puts "--------------".center(50)
 end
 
-def print_students_list(cohort_months)
+def print(students, cohort_months)
    linewidth = 100
+   if students.empty?
+      puts "Nothing to print yo!"
+   end
    cohort_months.each do |month|
-      @students.each_with_index do |student, index|
+      students.each_with_index do |student, index|
          # Or you can use student["name"] if your hash was set up like so:
          # "name" => "Darth Vader", etc. (Two ways of doing it)
          if student[:cohort] == month
@@ -89,27 +85,14 @@ def print_students_list(cohort_months)
    end
 end
 
-def print_footer
+def print_footer(names)
    linewidth = 100
    puts "--------------".center(50)
-   if @students.count == 1
+   if names.count == 1
       puts "Our grand total is 1 villainous student!".center(50)
    else
-      puts "Overall, we have #{@students.count} villainous students.".center(50)
+      puts "Overall, we have #{names.count} villainous students.".center(50)
    end
 end
 
 interactive_menu
-
-# We don't need these old method calls for now
-=begin
-students = input_students
-if students.empty?
-   puts "Nothing to print yo!"
-   exit
-end
-print_header
-cohort_months = cohorts(students)
-print(students, cohort_months)
-print_footer(students)
-=end
